ed0dbd0bcebcdffe4aebf1a2b0f6917f1289cf7f
nipunbharti <ced16i026@iiitdm.ac.in>
2019-02-09 12:53:49 +0530

    Added: Complete time slice api

--- routes/TimeSliced.js
+++ routes/TimeSliced.js
@@ -1,19 +1,43 @@
+const moment = require('moment');
+
+async function getObject(key) {
+	let params = {
+		Bucket: 'cromdev',
+		Key: key
+	};
+	let comp = await s3.getObject(params).promise();
+	comp["name"] = key;
+	return comp;
+}
+
 module.exports = (app, AWS) => {
 	let bucketParams = {
 		Bucket: 'cromdev'
 	};
 
-	app.post('/timeSlicedImages', (req, res) => {
-		let { body } = req;
-		let { time1, time2 } = body;
+	app.get('/timeSlicedImages', (req, res) => {
+		// let { body } = req;
+		// let { time1, time2 } = body;
 
 		s3 = new AWS.S3({apiVersion: '2019-02-09'});
-		s3.listObjects(bucketParams, function(err, data) {
+		s3.listObjects(bucketParams,async function(err, data) {
 		  if (err) {
 		    console.log("Error", err);
 		  } else {
-		  	return res.send(data);
-		    // console.log("Success", data);
+		  	var i=0;
+		  	// To be done
+			// let date1 = moment(time1, 'DDMMYYYYHHmm').format('DDMMYYYYHHmm');
+			// let date2 = moment(time2, 'DDMMYYYYHHmm').format('DDMMYYYYHHmm');
+			// console.log(date1, date2);
+			// let prunedData = data.Contents.map(data => data.Key.slice()); 
+			let namedData = data.Contents.map(data => data.Key);
+			let returnedData = [];
+			for(let i=0;i<namedData.length;i++) {
+				let res = await getObject(namedData[i]);
+				returnedData.push(res);
+			}
+			console.log(returnedData);
+			return res.send(returnedData);
 		  }
 		});
 	})	
