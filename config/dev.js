6899ed33f7520b81dfcdd0ff87c44c0ca764062a
nipunbharti <ced16i026@iiitdm.ac.in>
2019-04-06 19:13:27 +0530

    Removed cache

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://nipun:sinatra21@ds219879.mlab.com:19879/cromdev_pdp",
-    bucket: 'pdpdev2',
-    collectionName: 'pdpdev2',
-    'feedName': 'cam_a'
-};
