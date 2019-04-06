246c5d797324239e1794874a9d5711b26cc94ac1
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2019-04-06 10:31:02 +0530

    Rectified: Minor Fix

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,5 @@ module.exports = {
     mongoURI: "mongodb://nipun:sinatra21@ds219879.mlab.com:19879/cromdev_pdp",
     bucket: 'pdpdev1',
     collectionName: 'pdpdev1',
-    'feedName': 'cam_r'
+    'feedName': 'cam_d'
 };
